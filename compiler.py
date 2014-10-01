e082f92cbcdc59cb5d727c0c5936cc45f13c4618
Sam Heather <sam@heather.sh>
2014-10-01 23:50:32 +0100

    Adds support for labels and jumps, much more robust token handeling, better errors and better debugMode.  Generally cleaner code as well.  Fixes #4 and #7.

--- compiler.py
+++ compiler.py
@@ -1,12 +1,14 @@
+import sys
+
 compilerDictionary = {
 						"LOAD" : "00000000",
 						"INPUT" : "00000001",
 						"READ" : "00100000",
 						"WRITE" : "00100001",
-						"JUMP" : "00110000",
-						"JUMP" : "00110001",
-						"JUMP" : "00110010",
-						"JUMP" : "00110011",
+						"JUMPZ" : "00110000",
+						"JUMPNZ" : "00110001",
+						"JUMPC" : "00110010",
+						"JUMPNC" : "00110011",
 						"JUMP" : "00010000",
 						"ADD" : "00000100",
 						"SUB" : "00000101",
@@ -30,6 +32,7 @@ compilerDictionary = {
 
 standaloneOpcodes = ["INPUT", "SHIFTL", "SHIFTR", "ROTL", "ROTR"]
 jumpOpcodes = ["JUMPZ", "JUMPNZ", "JUMPC", "JUMPNC", "JUMP"]
+jumpOpcodesCompiled = []
 labels = {}
 
 # Debug mode > instructions on 1:1 new lines, with space between opcode/operand
@@ -39,19 +42,69 @@ debugMode = False
 outputFileName = 'googleIsCool.bin'
 
 def setupAndStart():
+	# Populate jumpOpcodesCompiled
+	for opcode in jumpOpcodes:
+		jumpOpcodesCompiled.append(compilerDictionary[opcode])
+
 	lines = [line.strip() for line in open('./program.uni')]
-	compiled = ""
+	compiled = []
+		
+	# Remove lines that are or will be blank
+	removeBlank(lines)
+	
+	if (len(lines) > 128):
+		print "ERROR: Programs can not have length greater than 128 lines. " \
+		+ "The theoretical limit of the hardware is 256 lines, however this " \
+		+ "compiler is limited due to the compileValue method (which, " \
+		+ "although messy, it is possible to fix)"
+		sys.exit(1)
+	
+	# COMPILE
 	for lineNumber, line in enumerate(lines):
 		line = line.upper()
-		compiled += compileLine(line, lineNumber+1)
+		compiled.append(compileLine(line, lineNumber))
+		
+	while (None in compiled):
+		compiled.remove(None)
+		
+	# LINK
+	for lineNumber in range(0, len(compiled)):
+		if (compiled[lineNumber][0] in jumpOpcodesCompiled):
+			try:
+				compiled[lineNumber][1] = compileValue(labels[compiled[lineNumber][1]], None)
+			except KeyError:
+				print "ERROR: LABEL: ", compiled[lineNumber][1], " is never defined."
+				sys.exit(1)
+	
+	# CONCATENATE
+	compiledString = concatenate(compiled, debugMode)
+	
 	print "UniCPUTeam Compiler - by Sam Heather, Charlie Ford and Andrei Zisu\n" \
 	+ "Please be aware, output to binary will only work when executing in Python environment <3.0\n" \
-	+ "Compiled code:\n------------------------------\n" + compiled \
-	+ "\n------------------------------"
-	if (outputToBinary(compiled)):
+	+ "Max program length: 128 lines (theoretical: 256, restriction caused by compileValue function in compiler)\n"
+	
+	if (debugMode):
+		print "Stripped original code (blank and comment-only lines removed): \n------------------------------"
+		for line in lines:
+			print line
+		print "------------------------------"
+		print "Labels used within code: \n", labels
+	
+	
+	print "Compiled code:\n------------------------------\n" + compiledString \
+	+ "\n------------------------------\n"
+
+	if (outputToBinary(compiledString)):
 		print 'Binary output to: ' + outputFileName
 	else:
-		print 'Debug mode, not writing binary file.\n'
+		print 'DEBUG mode - not writing binary file.'
+
+def removeBlank(lines):
+	numberRemoved = 0
+	for x in range(0, len(lines)):
+		if (removeComment(lines[x-numberRemoved]) == ''):
+			lines.pop(x-numberRemoved)
+			numberRemoved += 1
 
 def compileLine(inputLine, lineNumber):
 	''' str -> str '''
@@ -64,23 +117,22 @@ def compileLine(inputLine, lineNumber):
 		if not (tokens[0] in standaloneOpcodes):
 			raise ValueError('Expected operand for opcode: ' + tokens[0]) \
 			+ 'Line number: ' + str(lineNumber)
-		elif (debugMode):
-			return compileInstruction(tokens[0], lineNumber) + " 00000000\n"
 		else:
-			return compileInstruction(tokens[0], lineNumber) + "00000000"
+			return [compileInstruction(tokens[0], lineNumber), "00000000"]
 		 
 	elif len(tokens) == 2:	#expecting standard instruction
 		if (tokens[0] in standaloneOpcodes):
 			raise ValueError('Unexpected operand for opcode: ' + tokens[0]) \
 			+ 'Line number: ' + str(lineNumber)
+		elif (tokens[0] == 'LABEL'):
+			if (labels.has_key(tokens[1])):
+				raise ValueError('Can not re-define label: ' + tokens[1] + ' on line: ' + lineNumber)
+			labels[tokens[1]] = lineNumber-len(labels)
+			return None
 		elif (tokens[0] in jumpOpcodes):
 			return compileJump(tokens, lineNumber)
-		elif (debugMode):
-			return compileInstruction(tokens[0], lineNumber) + " " \
-			+ compileValue(int(tokens[1]), lineNumber) + "\n"
 		else:
-			return compileInstruction(tokens[0], lineNumber) \
-			+ compileValue(int(tokens[1]), lineNumber)
+			return [compileInstruction(tokens[0], lineNumber), compileValue(int(tokens[1]), lineNumber)]
 
 	else:	#too many operands!
 		raise ValueError('Unexpected operands for opcode: ' + tokens[0]) \
@@ -91,10 +143,7 @@ def removeComment(instruction):
 	return instruction.split("//")[0]
 
 def compileJump(inputTokens, lineNumber):
-	if (labels.has_key(inputTokens[1])):
-		raise ValueError('Can not re-define label: ' + inputTokens[1] + ' on line: ' + lineNumber)
-	labels[inputTokens[1]] = lineNumber
-	return compileInstruction(inputTokens[0], lineNumber) + inputTokens[1]
+	return [compileInstruction(inputTokens[0], lineNumber), inputTokens[1]]
 
 def compileInstruction(instruction, lineNumber):
 	''' str -> str '''
@@ -120,6 +169,8 @@ def compileValue(value, lineNumber):
 	else:
 		raise ValueError('Value does not fit into 8 bits.  ' \
 		+ 'Line number: ' + str(lineNumber))
+		
+# Helper functions
 		
 def bulkBinaryTo8Bits(input):
 	'''list -> list'''
@@ -150,7 +201,7 @@ def binaryIncrement(input):
 	
 def outputToBinary(compiledCode):
 	if (debugMode):
-		print 'Debug mode - not converting formatted string'
+		print 'DEBUG mode - not converting formatted string'
 		return False
 	outputData = ''
 	while (len(compiledCode) > 0):
@@ -160,5 +211,14 @@ def outputToBinary(compiledCode):
 	f = open(outputFileName, 'wb') #b means binary mode on Windows
 	f.write(outputData)
 	return True
+	
+def concatenate(input, debugMode):
+	output = ""
+	for line in input:
+		if not (debugMode):
+			output = output + line[0] + line[1]
+		else:
+			output = output + line[0] + " " + line[1] + "\n"
+	return output
 
 setupAndStart()
